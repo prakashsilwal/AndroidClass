373c372536908bd97f0ae5124016b444f2efd728
prakashsilwal <prakashsilwal61@gmail.com>
2020-12-04 10:28:37 +0545

    Initial commit

---
+++ app/src/main/java/com/prakash/anroidapp25a/simpleInterestActivity.kt
@@ -0,0 +1,11 @@
+package com.prakash.anroidapp25a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class simpleInterestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_interest)
+    }
+}
